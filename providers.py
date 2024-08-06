c6cb2f42fe3611ca538d1c99257469ac01cdaa11
Alexandre Velloso <alexandre.velloso@fanduel.com>
2024-08-06 11:39:11 +0100

    Print query

---
+++ providers.py
@@ -0,0 +1,4 @@
+from enum import Enum
+
+class Provider(Enum):
+    GENIUS=40
---
+++ sports.py
@@ -0,0 +1,51 @@
+from enum import Enum
+
+class Sport(Enum):
+    AMERICAN_FOOTBALL=6423
+    
+# -1	Other sports types
+# 1	Football
+# 7	Horseracing
+# 4339	Greyhounds Racing
+# 998918	Bowls
+# 7524	Ice Hockey
+# 7522	Basketball
+# 998917	Volleyball
+# 468328	Handball
+# 4	Cricket
+# 3503	Darts
+# 6422	Snooker
+# 2	Tennis
+# 3	Golf
+# 5	Rugby Union
+# 6	Boxing
+# 8	Motor Sport
+# 7511	Baseball
+# 1477	Rugby League
+# 998920	Floorball
+# 998919	Bandy
+# 627555	Badminton
+# 2872194	Beach Volleyball
+# 27454571	Esports
+# 10	Specials
+# 11	Cycling
+# 3988	Unknown Sport (3988)
+# 6423	American Football
+# 7523	Hockey
+# 61420	Australian Rules
+# 72382	Nine-Ball
+# 136332	Chess
+# 300000	Testing 1
+# 451485	Skiing
+# 620576	Unknown Sport (620576)
+# 2378961	Politics
+# 2901849	Water Polo
+# 3088925	Unknown Sport (3088925)
+# 4726642	Surfing
+# 15826206	Unknown Sport (15826206)
+# 26420387	Mixed Martial Arts
+# 27589895	Testing 2
+# 28347305	Unknown Sport (28347305)
+# 29125756	Lottery
+# 2593174	Table Tennis
+# 606611	Netball
