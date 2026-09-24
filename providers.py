79d61fe73b71e36bc316da1822d42a327b697bd5
Alexandre Velloso <alexandre.velloso@fanduel.com>
2026-09-24 11:48:49 +0100

    Fix

--- providers.py
+++ providers.py
@@ -1,7 +1,16 @@
 from enum import Enum
+from sports import Sport
 
 class Provider(Enum):
+    PERFORM_V2_OTHER=31
     BETRADAR_V2=33
+    BETRADAR_V3=34
     IMG_GOLF=36
     PERFORM_MCC_VIDEO=38
     GENIUS=40
+
+class ProviderClass:
+    
+    def __init__(self, id, provider_sports_type):
+        self.id = id
+        self.provider_sports_type
